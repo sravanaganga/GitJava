ed7ee6f5e13ef3517ae2e5209ac213ee5ccd6114
sravanaganga <sravanaganga.kudaravalli@gmail.com>
2016-01-06 17:27:02 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,13 +1,7 @@
-import java.lang.*;
-
 public class First
  {
   public int square(int num)
    {
     return num*num;
-  }
-  public static void main(String args[])
-  {
-   System.out.println("JUnitTesting");
   }
 }
